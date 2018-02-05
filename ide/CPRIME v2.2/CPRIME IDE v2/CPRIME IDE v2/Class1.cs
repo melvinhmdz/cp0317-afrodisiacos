5db03a19613e5f419a9ccac05c3588974b17395c
melvinMdz <34014901+melvinMdz@users.noreply.github.com>
2018-02-04 22:39:00 -0600

    Nuevas funciones
    - Tabs independientes para abrir y guardar
    
    -cada tab abierto marca en colo las palabras reservadas.

---
+++ ide/CPRIME v2.2/CPRIME IDE v2/CPRIME IDE v2/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPRIME_IDE_v2
+{
+    class Class1
+    {
+        public string token;
+        public string valor;
+        public string filcol;
+
+        public string Token
+        {
+            get
+            {
+                return this.token;
+            }
+            set
+            {
+                this.token = value;
+            }
+        }
+        public string Valor
+        {
+            get
+            {
+                return this.valor;
+            }
+            set
+            {
+                this.valor = value;
+            }
+        }
+
+        public string Filcol
+        {
+            get
+            {
+                return this.filcol;
+            }
+            set
+            {
+                this.filcol = value;
+            }
+        }
+
+
+    }
+}
